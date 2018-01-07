e4c422a3b7be95b5dd52996f19a11b1abbec09d4
mthdht <mthdht@gmail.com>
2018-01-07 21:48:31 +0100

    finish view for adding score

--- resources/views/scores/create.blade.php
+++ resources/views/scores/create.blade.php
@@ -2,9 +2,75 @@
 
 @section('content')
 
-    <div class="panel-heading">Add New Score ! </div>
+    <h2 class="panel-heading">Add New Score ! </h2>
 
     <div class="panel-body">
+        <form class="form-horizontal" method="POST" action="{{ route('scores.store') }}">
+            {{ csrf_field() }}
 
+            <div class="form-group{{ $errors->has('buyIn') ? ' has-error' : '' }}">
+                <label for="buyIn" class="col-md-4 control-label">Buy In</label>
+
+                <div class="col-md-6">
+                    <input id="buyIn" type="number" class="form-control" name="buyIn" value="{{ old('buyIn') }}" required autofocus>
+
+                    @if ($errors->has('buyIn'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('buyIn') }}</strong>
+                        </span>
+                    @endif
+                </div>
+            </div>
+
+            <div class="form-group{{ $errors->has('mise') ? ' has-error' : '' }}">
+                <label for="buyIn" class="col-md-4 control-label">Mise</label>
+
+                <div class="col-md-6">
+                    <input id="mise" type="number" class="form-control" name="mise" value="{{ old('mise') }}" required >
+
+                    @if ($errors->has('mise'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('mise') }}</strong>
+                        </span>
+                    @endif
+                </div>
+            </div>
+
+            <div class="form-group{{ $errors->has('gains') ? ' has-error' : '' }}">
+                <label for="buyIn" class="col-md-4 control-label">Gains</label>
+
+                <div class="col-md-6">
+                    <input id="gains" type="number" class="form-control" name="gains" value="{{ old('gains') }}" required >
+
+                    @if ($errors->has('gains'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('gains') }}</strong>
+                        </span>
+                    @endif
+                </div>
+            </div>
+
+            <div class="form-group{{ $errors->has('datePartie') ? ' has-error' : '' }}">
+                <label for="datePartie" class="col-md-4 control-label">Date de la partie</label>
+
+                <div class="col-md-6">
+                    <input id="datePartie" type="date" class="form-control" name="datePartie" value="{{ old('datePartie') }}" required >
+
+                    @if ($errors->has('datePartie'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('datePartie') }}</strong>
+                        </span>
+                    @endif
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="col-md-6 col-md-offset-4">
+                    <button type="submit" class="btn btn-primary">
+                        Register
+                    </button>
+                </div>
+            </div>
+        </form>
     </div>
 @endsection
