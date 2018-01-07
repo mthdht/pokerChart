00fca0b641c62eb2efa36136a6e83b8f8c0e13c7
mthdht <mthdht@gmail.com>
2018-01-07 14:45:20 +0100

    score.php (model) -> add user method to access user from score

--- app/Score.php
+++ app/Score.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Score extends Model
 {
-    //
+    /**
+     * Get the post that owns the comment.
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
