91b0f8265b48ce7a6da66f49199d9e9c010a21c5
mthdht <mthdht@gmail.com>
2018-01-07 14:44:17 +0100

    User.php (model) -> add scores method to access user scores

--- app/User.php
+++ app/User.php
@@ -26,4 +26,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+    * Get the score(s) for the user.
+    */
+    public function scores()
+    {
+        return $this->hasMany('App\Score');
+    }
 }
