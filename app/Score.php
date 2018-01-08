033edcd2013b32f9dd59f236cd52cdd0ed027482
mthdht <mthdht@gmail.com>
2018-01-08 13:26:57 +0100

    make mass assignement on scores

--- app/Score.php
+++ app/Score.php
@@ -6,6 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Score extends Model
 {
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'buyIn', 'mise', 'gains', 'datePartie',
+    ];
+
     /**
      * Get the post that owns the comment.
      */
