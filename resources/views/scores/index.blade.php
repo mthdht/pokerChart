7822f9d039959823916c24c5b88734e18db6262e
mthdht <mthdht@gmail.com>
2018-01-08 17:25:35 +0100

    change score index to table

--- resources/views/scores/index.blade.php
+++ resources/views/scores/index.blade.php
@@ -1,51 +1,48 @@
 @extends('layouts.app')
 
 @section('content')
-    @foreach($scores as $score)
-        <h2 class="panel-heading "><span class="text-muted"><b>Ton score, partie du {{ $score->datePartie }}!</b></span> </h2>
 
-        <div class="panel-body ">
-            <div class="row">
-                <div class="col-xs-7 container">
-                    <div class="">
-                        <label class="col-xs-5 ">Buy-in</label>
-                        <p class="col-xs-7 text-right">{{ $score->buyIn }}</p>
-                    </div>
-                    <div class="clearfix"></div>
-                    <div class="">
-                        <label class="col-xs-5 ">Mise</label>
-                        <p class="col-xs-7 text-right">{{ $score->mise }}</p>
-                    </div>
-                    <div class="clearfix"></div>
-                    <div>
-                        <label class="col-xs-5 ">Gains</label>
-                        <p class="col-xs-7 text-right">{{ $score->gains }}</p>
-                    </div>
-                    <div class="clearfix"></div>
-                    <div>
-                        <label class="col-xs-5">Bénéfice</label>
-                        <p class="col-xs-7 text-right">{{ $score->benefice }}</p>
-                    </div>
-                    <div class="clearfix"></div>
-                    <div>
-                        <label class="col-xs-5">Re-cave</label>
-                        <p class="col-xs-7 text-right">{{ $score->recave }}</p>
-                    </div>
-                </div>
-                <div class="col-xs-5 container">
-                    <a href="{{ route('scores.edit', ['id' => $score->id]) }}" class="btn btn-warning ">Editer</a>
-                    <form method="POST" action="{{ route('scores.destroy', ['id' => $score->id]) }}">
-                        {{ method_field('DELETE') }}
-                        {{ csrf_field() }}
-                        <button class="btn btn-danger" type="submit">Supprimer</button>
-                    </form>
-
-                </div>
-            </div>
-
+    <h2 class="panel-heading "><span class="text-muted"><b>Tous les scores!</b></span> </h2>
+    <div class="panel-body">
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Buy-In</th>
+                        <th>Mise</th>
+                        <th>Gains</th>
+                        <th>Bénefices</th>
+                        <th>Re-cave</th>
+                        <th>Date partie</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                @foreach($scores as $score)
+                    <tr class="{{ $score->benefice >= 0 ? $score->benefice > 0 ? 'success': '' : 'danger' }}">
+                        <td>{{ $score->id }}</td>
+                        <td>{{ $score->buyIn }}</td>
+                        <td>{{ $score->mise }}</td>
+                        <td>{{ $score->gains }}</td>
+                        <td>{{ $score->benefice }}</td>
+                        <td>{{ $score->recave }}</td>
+                        <td>{{ $score->datePartie }}</td>
+                        <td>
+                            <a href="{{ route('scores.edit', ['id' => $score->id]) }}" class="btn btn-warning ">Editer</a>
+                            <form method="POST" action="{{ route('scores.destroy', ['id' => $score->id]) }}">
+                                {{ method_field('DELETE') }}
+                                {{ csrf_field() }}
+                                <button class="btn btn-danger" type="submit">Supprimer</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
         </div>
-        <hr>
-    @endforeach
+    </div>
+
 @endsection
 
 @section('sideContent')
