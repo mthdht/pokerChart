7101a9f55d925a2d67af502ab3ac694e6ab2fd0a
mthdht <mthdht@gmail.com>
2018-01-15 13:55:37 +0100

    complete top cards stats

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,21 +12,20 @@
             @endif
 
             <div class="rapid row">
-                <div class="col-xs-6 col-md-3">
+                <div class="col-xs-6 col-md-4">
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             Dernier jeu
-                            <a href=""><i class="fa fa-2x fa-arrow-circle-o-right pull-right" aria-hidden="true"></i></a>
                         </div>
 
                         <div class="panel-body">
-                            <div class="row">
-                            <span class="col-xs-5">
-                                <i class="fa fa-3x fa-money" aria-hidden="true"></i>
-                            </span>
+                            <div class="row {{ $scoresOrder->last()->benefice >= 0 ? 'text-success' : 'text-danger' }}">
+                                <span class="col-xs-5">
+                                    <i class="fa fa-3x fa-money" aria-hidden="true"></i>
+                                </span>
 
                                 <span class="col-xs-7"><b>
-                                Gains <br><i class="fa fa-dollar" aria-hidden="true"></i></b>
+                                Gains <br> {{ $scoresOrder->last()->benefice }}<i class="fa fa-dollar" aria-hidden="true"></i></b>
                             </span>
                             </div>
                         </div>
@@ -34,21 +33,21 @@
                     </div>
                 </div>
 
-                <div class="col-xs-6 col-md-3">
+                <div class="col-xs-6 col-md-4">
                     <div class="panel panel-default">
                         <div class="panel-heading">
-                            Voir Stats
-                            <a href=""><i class="fa fa-2x fa-arrow-circle-o-right pull-right" aria-hidden="true"></i></a>
+                            toutes Parties
                         </div>
 
                         <div class="panel-body">
-                            <div class="row">
+                            <div class="row {{ $scoresOrder->sum('benefice') >= 0 ? 'text-success' : 'text-danger' }}">
                             <span class="col-xs-5">
                                 <i class="fa fa-3x fa-eur" aria-hidden="true"></i>
                             </span>
 
                                 <span class="col-xs-7 ">
                                 <b>Bénéfice<br>
+                                    {{ $scoresOrder->sum('benefice') }}
                                     <i class="fa fa-dollar" aria-hidden="true"></i></b>
                             </span>
                             </div>
@@ -57,50 +56,40 @@
                     </div>
                 </div>
 
-                <div class="col-xs-6 col-md-3">
+                <div class="col-xs-6 col-md-4">
                     <div class="panel panel-default">
                         <div class="panel-heading">
-                            Voir Stats
-                            <a href=""><i class="fa fa-2x fa-arrow-circle-o-right pull-right" aria-hidden="true"></i></a>
+                            Gains / mise
                         </div>
 
                         <div class="panel-body">
-                            <div class="row">
+                            <div class="row {{ $scoresOrder->sum('benefice') / $scoresOrder->sum('mise') >= 1 ? 'text-success' : 'text-danger' }}">
                             <span class="col-xs-5">
                                 <i class="fa fa-3x fa-area-chart" aria-hidden="true"></i>
                             </span>
 
                                 <span class="col-xs-7 ">
                                 <b>Ratio<br>
-                                    <i class="fa fa-dollar" aria-hidden="true"></i></b>
+                                    {{ round($scoresOrder->sum('gains') / $scoresOrder->sum('mise'), 2) }}
+                                    <i class="fa " aria-hidden="true"></i></b>
                             </span>
                             </div>
                         </div>
 
                     </div>
                 </div>
+            </div>
 
-                <div class="col-xs-6 col-md-3">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            Voir Stats
-                            <a href=""><i class="fa fa-2x fa-arrow-circle-o-right pull-right" aria-hidden="true"></i></a>
-                        </div>
-
-                        <div class="panel-body">
-                            <div class="row">
-                            <span class="col-xs-5">
-                                <i class="fa fa-3x fa-area-chart" aria-hidden="true"></i>
-                            </span>
-
-                                <span class="col-xs-7 ">
-                                <b>Bénéfice<br>
-                                    <i class="fa fa-dollar" aria-hidden="true"></i></b>
-                            </span>
-                            </div>
-                        </div>
-
+            <div class="beneficesPerGame panel panel-warning">
+                <div class="panel-heading">
+                    <div class="panel-title text-center">
+                        <b class="col-xs-8 ">Bénéfice par partie </b>
+                        <a href="{{ route('scores.index') }}" class="btn btn-info"> Voir les scores</a>
                     </div>
+                </div>
+
+                <div class="panel-body">
+                    <div id="beneficesPerGameChart" class="panel-body"></div>
                 </div>
             </div>
 
@@ -114,51 +103,35 @@
 
                 <div class="panel-body">
                     <div class="table-responsive">
-                        <table class="table">
-                            <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Buy-In</th>
-                                <th>Mise</th>
-                                <th>Gains</th>
-                                <th>Bénefices</th>
-                                <th>Re-cave</th>
-                                <th>Date partie</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @foreach($lastScores as $score)
-                                <tr class="{{ $score->benefice >= 0 ? $score->benefice > 0 ? 'success': '' : 'danger' }}">
-                                    <td>{{ $score->id }}</td>
-                                    <td>{{ $score->buyIn }}</td>
-                                    <td>{{ $score->mise }}</td>
-                                    <td>{{ $score->gains }}</td>
-                                    <td>{{ $score->benefice }}</td>
-                                    <td>{{ $score->recave }}</td>
-                                    <td>{{ $score->datePartie }}</td>
+                            <table class="table">
+                                <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Buy-In</th>
+                                    <th>Mise</th>
+                                    <th>Gains</th>
+                                    <th>Bénefices</th>
+                                    <th>Re-cave</th>
+                                    <th>Date partie</th>
                                 </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-
+                                </thead>
+                                <tbody>
+                                @foreach($lastScores as $score)
+                                    <tr class="{{ $score->benefice >= 0 ? $score->benefice > 0 ? 'success': '' : 'danger' }}">
+                                        <td>{{ $score->id }}</td>
+                                        <td>{{ $score->buyIn }}</td>
+                                        <td>{{ $score->mise }}</td>
+                                        <td>{{ $score->gains }}</td>
+                                        <td>{{ $score->benefice }}</td>
+                                        <td>{{ $score->recave }}</td>
+                                        <td>{{ $score->datePartie }}</td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
                 </div>
             </div>
-
-            <div class="lastGame panel panel-warning">
-                <div class="panel-heading">
-                    <div class="panel-title text-center">
-                        <b class="col-xs-8 ">Bénéfice par partie </b>
-                        <a href="{{ route('scores.index') }}" class="btn btn-info"> Voir les scores</a>
-                    </div>
-                </div>
-
-                <div class="panel-body">
-                    <div id="beneficesPerGameChart" class="panel-body"></div>
-                </div>
-            </div>
-
-
         </div>
     </div>
 @endsection
